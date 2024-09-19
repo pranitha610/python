de18a1c7947844d4ede965bc9bd298b9c0ffe641
pranitha610 <pranithapranitha610@gmail.com>
2024-09-19 19:53:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,3 +11,19 @@ def hello():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(debug=True,host='0.0.0.0',port=port)
+
+
+
+
+#first install python 
+#then python3.pip
+#install [pip install flask]
+#clone the git repo
+# cd----ls---python
+#vi requirement.txt
+#numpy==1.21.2
+pandas==1.3.3
+flask==2.0.1
+# python3 app.py 
+# copy the private ip in the new webb
+
