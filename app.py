36f9743d6a436c6e78310bd4e3b6b0915bc4702d
pranitha610 <pranithapranitha610@gmail.com>
2024-09-19 19:39:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
-from flask import *
+from flask import Flask
+import os
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return "<h1>Welcome to Jenkins Tutorials<h1/>"
+@app.route("/")
+def hello():
+    return "Flask inside Docker!!"
 
-if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0')
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True,host='0.0.0.0',port=port)
